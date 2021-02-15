5dd5393bd1dbcf5d01618da699c07253b0542960
Jordan Lambrecht <jordan@pixelbakery.co>
2021-02-15 16:30:40 -0600

    stylesheet cleanup & organization

--- inc/header.php
+++ inc/header.php
@@ -25,6 +25,7 @@
   <title>Portfolio Waterfall</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   <!-- <link href="../assets/css/cover.css" rel="stylesheet"></link> -->
+  <link rel="stylesheet" href="./assets/css/svg_colors.css">
   <link rel="stylesheet" href="./assets/css/style2.css">
 </head>
 <body>
