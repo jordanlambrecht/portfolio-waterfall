0e9bb320f38035deb5a162df41932b51d88872f9
Jordan Lambrecht <jordan@pixelbakery.co>
2021-02-15 19:11:38 -0600

    Added favicon

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,5 @@
 
- 
+
 <!DOCTYPE html>
 <html lang="en" >
 <head>
@@ -7,10 +7,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="">
   <meta name="author" content="Pixel Bakery">
+  <link rel="icon" type="image/png" href="PW_favicon.png" />
   <title>Portfolio Waterfall</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   <!-- <link href="../assets/css/cover.css" rel="stylesheet"></link> -->
-  <link rel="stylesheet" href="./assets/css/svg_colors.css">
+  <link rel="stylesheet" href="./assets/css/colors.css">
   <link rel="stylesheet" href="./assets/css/style2.css">
 </head>
 <body>
