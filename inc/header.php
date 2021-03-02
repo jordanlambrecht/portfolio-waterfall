028fa2441ae7442ce8240649fb3ca3ad057a9f28
Jordan Lambrecht <jordan@pixelbakery.co>
2021-03-02 12:04:05 -0600

    Removed viewport tool

--- inc/header.php
+++ inc/header.php
@@ -10,10 +10,13 @@
   <meta name="author" content="Pixel Bakery">
   <link rel="icon" type="image/png" href="PW_favicon.png" />
   <title>Portfolio Waterfall</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-  <link ref="stylesheet" href="./assets/css/flickity.css" />
+<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
   <link rel="stylesheet" href="./assets/css/colors.css">
-  <link rel="stylesheet" href="./assets/css/style2.css">
+  <link rel="stylesheet" href="./assets/css/pb-style.css">
+  <link rel="stylesheet" href="./assets/css/backtotop.css">
+  <link rel="stylesheet" href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1462889/unicons.css" />
 </head>
 <body>
-  <div class="screeninfo"></div>
+  <!-- <div class="screeninfo"></div> -->
