93a9ffc2e596d86d97afa0ef17b39c19cb5d0d4c
Jordan Lambrecht <jordan@pixelbakery.co>
2021-03-24 03:39:17 -0500

    Centered content, fixed jump bug in animation

--- inc/footer.php
+++ inc/footer.php
@@ -1,64 +1,89 @@
 </main>
 <footer class="footer">
-  <section class="footer-container pt-1 px-2 d-flex justify-content-start">
-    <div class="container d-flex flex-column justify-content-between mb-2 mt-0 pt-1">
-      <div class="row pb-1 ">
-        <div class="col-12 col-sm-12 col-md-6 col-lg-4 footer-content py-2  pt-sm-3 py-md-5">
-          <h1 class="text-uppercase color--white display-2 fw-b text-center text-md-start">Don't Let Your Portfolio Dry Up.</h1>
+  <section class="container-fluid h-100 footer-container py-1 h-100">
+    <div class="container py-1  h-100">
+
+
+      <div class="row h-100 px-2 d-flex flex-column justify-content-between">
+
+        <div class=" d-flex justify-content-center align-items-center flex-fill">
+          <h1 class="text-uppercase color--white display-2 fw-b text-center" id="footerdry">Don't Let Your Portfolio Dry Up.</h1>
         </div>
-        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-5 offset-lg-2 mb-2 mt-0 pt-1 d-flex flex-column">
-          <div class="row d-flex justify-content-center">
-            <div class="col-12">
-              <div class="footer-card px-5 py-5 my-4 shadow-sm">
-                <a href="/"><img class="img-fluid" src="https://portfoliowaterfall.pbds.dev/assets/img/PW_PrimaryLogo_Dark_v01.svg" /></a>
-              </div>
+
+        <div class="  d-flex justify-content-center flex-column align-items-center flex-fill">
+          <div class="footer-wrapper-logo">
+            <a href="/">
+              <img id="footer-logo" class="img-fluid bg--white p-4 rounded-2" src="../assets/img/PW_PrimaryLogo_Dark_v01.svg" />
+            </a>
+            <h2 id="footer-tagline" class="color--white text-uppercase fw-normal fst-italic mt-3 text-center">A Replenishing Income Strategy</h2>
+          </div>
+        </div>
+
+        <div class=" d-flex  justify-content-center px-sm-5">
+          <div class="px-lg-5 d-flex  justify-content-between justify-content-xxl-center h-100 footer-icon-panel align-items-center">
+
+
+            <div class="footer-icon-wrapper mb-2 px-2 px-md-4">
+              <img class="img-fluid shadow-sm" src="../assets/img/PW_marketVolatilityCard.svg" alt="Portfolio Waterfall Fixes Market Volatility" />
+            </div>
+            <div class="footer-icon-wrapper mb-2 px-2 px-md-4">
+              <img class="img-fluid shadow-sm" src="../assets/img/PW_interestRatesCard.svg" alt="Portfolio Waterfall Fixes Low Interest Rates" />
+            </div>
+            <div class="footer-icon-wrapper mb-2 px-2 px-md-4">
+              <img class="img-fluid shadow-sm" src="../assets/img/PW_longevityCard.svg" alt="Portfolio Waterfall Fixes Market Longevity" />
             </div>
           </div>
-          <div class="row d-flex justify-content-center ">
-            <div class="col-4 mb-2 h-100">
-              <div class="footer-icon-outer bg--white rounded-3 h-100 px-3 py-3 d-flex align-items-center">
-                <div class=" p-1 footer-icons-inner   border--light-green  ">
-                  <img  alt="stock market down" src="https://portfoliowaterfall.pbds.dev/assets/img/PW_Clock.png" class="img-fluid p-2" />
-                </div>
-              </div>
-            </div>
-            <div class="col-4 mb-2 h-100">
-              <div class="footer-icon-outer bg--white rounded-3 h-100 px-3 py-3 d-flex align-items-center">
-                <div class=" p-1 footer-icons-inner   border--light-green  ">
-                  <img  alt="stock market down" src="https://portfoliowaterfall.pbds.dev/assets/img/PW_Down.png" class="img-fluid p-2" />
-                </div>
-              </div>
-            </div>
-            <div class="col-4 mb-2 h-100">
-              <div class="footer-icon-outer bg--white rounded-3 h-100 px-3 py-3 d-flex align-items-center">
-                <div class=" p-1 footer-icons-inner   border--light-green  ">
-                  <img  alt="stock market down" src="https://portfoliowaterfall.pbds.dev/assets/img/PW_Stocks.png" class="img-fluid p-2" />
-                </div>
-              </div>
-            </div>
-          </div>
+
+
+        </div>
+        <div class=" py-3 d-flex flex-column justify-content-center ">
+
+            <small class="footer-copyright shadow-sm w-100 text-center">© Copyright <?php echo Date("Y");?>, All Rights Reserved.</small>
+
+
+
+            <small class="footer-copyright shadow-sm text-center">Website Made In Nebraska With Love By <a href="https://pixelbakery.com" target="_blank" class="">Pixel Bakery Design Studio</a>. </small>
+
         </div>
       </div>
+    </div>
+  </section>
+</footer>
+            <!-- <div class="row">
+              <div class="footer-icon-outer bg--white rounded-3 h-100 px-3 py-3 d-flex align-items-center">
+
+              </div>
+            </div>
+            <div class="col-4 mb-2 h-100">
+              <div class="footer-icon-outer bg--white rounded-3 h-100 px-3 py-3 d-flex align-items-center">
+                <div class=" p-1 footer-icons-inner   border--light-green  ">
+                  <img  alt="stock market down" src="./assets/img/PW_Down.png" class="img-fluid p-2" />
+                </div>
+              </div>
+            </div>
+            <div class="col-4 mb-2 h-100">
+              <div class="footer-icon-outer bg--white rounded-3 h-100 px-3 py-3 d-flex align-items-center">
+                <div class=" p-1 footer-icons-inner   border--light-green  ">
+                  <img  alt="stock market down" src="./assets/img/PW_Stocks.png" class="img-fluid p-2" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> -->
       <!-- Copyright Info -->
-      <div class="row  ">
+      <!-- <div class="row  ">
         <div class="col-12 text-center py-3 d-flex flex-column justify-content-center">
           <div class="text-center w-100">
             <small class="footer-copyright shadow-sm w-100 text-center">© Copyright <?php echo Date("Y");?>, All Rights Reserved.</small>
           </div>
           <small class="footer-copyright shadow-sm">Website Made In Nebraska With Love By <a href="https://pixelbakery.com" target="_blank" class="">Pixel Bakery Design Studio</a>. </small>
-          <p class="w-100 ">
 
 
-          </p>
-          <p class=" fw-light lh-1 my-0">
-
-          </p>
         </div>
 
-      </div>
-    </div>
-  </section>
-</footer>
+      </div> -->
+
 <div class="progress-wrap">
   <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
     <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"/>
