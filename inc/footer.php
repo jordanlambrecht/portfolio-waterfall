a7823ffc959a20430a18d6f0e1c307f80a8be391
Jordan Lambrecht <jordan@pixelbakery.co>
2021-04-14 18:22:42 -0500

    Production Pre-Flight: Deleted unused HTML and comments

--- inc/footer.php
+++ inc/footer.php
@@ -2,25 +2,18 @@
 <footer class="footer">
   <section class="container-fluid h-100 footer-container py-1 h-100">
     <div class="container py-1  h-100">
-
-
       <div class="row h-100 px-2 d-flex flex-column justify-content-between">
-
         <div class=" d-flex justify-content-center align-items-center flex-fill">
           <h1 class="text-uppercase color--white display-2 fw-b text-center" id="footerdry">Don't Let Your Portfolio Dry Up.</h1>
         </div>
-
         <div class="  d-flex justify-content-center flex-column align-items-center flex-fill row">
           <div class="col-12 p-5 p-md-2 footer-wrapper-logo d-flex flex-column justify-content-center align-items-center">
             <img id="footer-logo" class="img-fluid" src="../assets/img/PortfolioWaterfall_PrimaryLogo_v01@2x.png" />
             <h2 id="footer-tagline" class="color--white text-uppercase fst-italic mt-4 text-center">A Replenishing<br>  Income Strategy</h2>
           </div>
         </div>
-
         <div class=" d-flex  justify-content-center px-sm-5">
           <div class="px-lg-5 d-flex  justify-content-between justify-content-xxl-center h-100 footer-icon-panel align-items-center">
-
-
             <div class="footer-icon-wrapper mb-2 px-2 px-md-4">
               <img class="img-fluid shadow-sm" src="../assets/img/PW_marketVolatilityCard.svg" alt="Portfolio Waterfall Fixes Market Volatility" />
             </div>
@@ -31,57 +24,15 @@
               <img class="img-fluid shadow-sm" src="../assets/img/PW_longevityCard.svg" alt="Portfolio Waterfall Fixes Market Longevity" />
             </div>
           </div>
-
-
         </div>
         <div class=" py-3 d-flex flex-column justify-content-center ">
-
             <small class="footer-copyright shadow-sm w-100 text-center">© Copyright <?php echo Date("Y");?>, All Rights Reserved.</small>
-
-
-
             <small class="footer-copyright shadow-sm text-center">Website Made In Nebraska With Love By <a href="https://pixelbakery.com" target="_blank" class="">Pixel Bakery Design Studio</a>. </small>
-
         </div>
       </div>
     </div>
   </section>
 </footer>
-            <!-- <div class="row">
-              <div class="footer-icon-outer bg--white rounded-3 h-100 px-3 py-3 d-flex align-items-center">
-
-              </div>
-            </div>
-            <div class="col-4 mb-2 h-100">
-              <div class="footer-icon-outer bg--white rounded-3 h-100 px-3 py-3 d-flex align-items-center">
-                <div class=" p-1 footer-icons-inner   border--light-green  ">
-                  <img  alt="stock market down" src="./assets/img/PW_Down.png" class="img-fluid p-2" />
-                </div>
-              </div>
-            </div>
-            <div class="col-4 mb-2 h-100">
-              <div class="footer-icon-outer bg--white rounded-3 h-100 px-3 py-3 d-flex align-items-center">
-                <div class=" p-1 footer-icons-inner   border--light-green  ">
-                  <img  alt="stock market down" src="./assets/img/PW_Stocks.png" class="img-fluid p-2" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> -->
-      <!-- Copyright Info -->
-      <!-- <div class="row  ">
-        <div class="col-12 text-center py-3 d-flex flex-column justify-content-center">
-          <div class="text-center w-100">
-            <small class="footer-copyright shadow-sm w-100 text-center">© Copyright <?php echo Date("Y");?>, All Rights Reserved.</small>
-          </div>
-          <small class="footer-copyright shadow-sm">Website Made In Nebraska With Love By <a href="https://pixelbakery.com" target="_blank" class="">Pixel Bakery Design Studio</a>. </small>
-
-
-        </div>
-
-      </div> -->
-
 <div class="progress-wrap">
   <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
     <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"/>
@@ -97,10 +48,7 @@
 <script src="./assets/js/showSlides.js"></script>
 <script src="./assets/js/gsap_scripts.js"></script>
 <script src="./assets/js/backtotop.js"></script>
-<!-- <script src="./assets/js/panelsnap.js"></script> -->
 <script src="./assets/js/general_scripts.js"></script>
-
-<!-- <script type="text/javascript" src="fullpage.js"></script> -->
 
 </body>
 </html>
