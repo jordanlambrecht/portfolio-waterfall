afc2c9b878c6eca8eb5c3ac412832f1a177a3f2c
Jordan Lambrecht <jordan@pixelbakery.co>
2021-04-15 09:16:26 -0500

    Production Pre-Flight: Indented All HTML

--- inc/section_02_whatLimits.php
+++ inc/section_02_whatLimits.php
@@ -7,7 +7,6 @@
       </div>
     </div>
     <div class="row d-md-flex justify-content-md-start">
-
       <div class="col-12 col-lg-5 responsive-border-bottom h-100 d-md-flex flex-column justify-content-end align-items-center ms-md-n3 ps-md-n1 py-2" id="limits-card-01">
         <div class="limits-card d-flex flex-column justify-content-between px-3 py-4 mx-3 mb-4 shadow">
           <h3 class="fw-bold color--dark-blue mb-3 ps-2 pe-5 mb-2 mt-1">Traditional Retirement Income Withdrawal Rate <span class="ft-italic color--black">IN 1995:</span></h3>
@@ -55,11 +54,9 @@
         <div class="image-column mt-3 mb-5 align-self-stretch py-1">
         <?php echo file_get_contents("./assets/img/PW_GovtSpending_v01.svg"); ?>
         </div>
-
-
       </div>
 
-<!-- Bottom/Left check -->
+      <!-- Bottom/Left check -->
       <div class="col-12 col-lg-5 responsive-border-bottom h-100 d-md-flex flex-column justify-content-end align-items-center ms-md-n3 ps-md-n1 py-2" id="limits-card-02">
         <div class="limits-card d-flex flex-column justify-content-between px-3 py-4 mx-3 mb-4 shadow">
           <h3 class="fw-bold color--dark-blue mb-3 ps-2 pe-5 mb-2 mt-1">Traditional Retirement Income Withdrawal Rate <span class="ft-italic color--black">TODAY:</span></h3>
