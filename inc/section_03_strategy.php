4042579aef0977fdcbdbe7fd35d724561411dc8c
Jordan Lambrecht <jordan@pixelbakery.co>
2021-04-19 19:06:56 -0500

    Added animation to the section header

--- inc/section_03_strategy.php
+++ inc/section_03_strategy.php
@@ -1,12 +1,11 @@
 <section class="chapter container-fluid d-none d-lg-block vh-100 p-0 m-0 "  id="strategy" data-section-name="The PF Method">
-
-<div class="m-0 h-100 d-flex flex-column">
-  <div class="bg--light-blue m-0 row pb-3 w-100">
-    <div class="col-12 mt-1 mb-1 col-12 text-center d-flex justify-content-center">
-      <h1 class="display-2 color--white text-center mw-100" id="strategyHeader">WHAT LIMITS WITHDRAWAL RATES?</h1>
+  <div class="m-0 h-100 d-flex flex-column">
+    <div class="bg--light-blue m-0 row pb-3 w-100">
+      <div class="col-12 mt-1 mb-1 col-12 text-center d-flex justify-content-center">
+        <h1 class="display-2 color--white text-center mw-100" id="strategyHeader">WHAT LIMITS WITHDRAWAL RATES?</h1>
+      </div>
     </div>
-  </div>
-  <div class=" py-0 row d-md-flex justify-content-md-between flex-fill my-0">
+    <div class=" py-0 row d-md-flex justify-content-md-between flex-fill my-0">
     <!-- Market Volatility -->
       <div class="col col-sm-4 bg--lightblue-to-darkblue h-100 d-sm-flex flex-column justify-content-between align-items-center px-3 pb-4 pt-4 flex-fill h-100" id="stratcard-01-wrapper">
         <div class="mt-3 mb-1 pb-5 px-2 w-75 ">
@@ -43,23 +42,22 @@
           </div>
 
       <!-- Longevity -->
-          <div class="col col-sm-4 bg--lightblue-to-lightyellow h-100 d-sm-flex flex-column justify-content-end justify-content-between align-items-center px-3 pb-4 pt-3 flex-fill h-100"  id="stratcard-03-wrapper">
-            <div class="mt-3 pb-5 px-2 w-75 mb-1 ">
-              <div class="strategy-card w-100 shadow " data-content="LONGEVITY" id="stratcard-03">
-                <div class="d-flex w-100 h-100 justify-content-center align-items-center p-2">
-                    <?php echo file_get_contents("./assets/img/PF_Icon_Longevity.svg"); ?>
-                </div>
-              </div>
-            </div>
-            <div class="line-down flex-fill">  </div>
-            <div class="general-card shadow py-2 px-3 mt-2 mb-4 w-75">
-              <img src="./assets/img/PW_PrimaryLogo_Dark_v01.svg" alt="Portfolio Waterfall Logo" class="img-fluid pb-3 px-4" />
-              <p class="text-center fst-italic px-3 fs-5 fw-bold lh-1 color--black">
-                Creates a Replenishing<br /> Lifetime Income
-              </p>
+      <div class="col col-sm-4 bg--lightblue-to-lightyellow h-100 d-sm-flex flex-column justify-content-end justify-content-between align-items-center px-3 pb-4 pt-3 flex-fill h-100"  id="stratcard-03-wrapper">
+        <div class="mt-3 pb-5 px-2 w-75 mb-1 ">
+          <div class="strategy-card w-100 shadow " data-content="LONGEVITY" id="stratcard-03">
+            <div class="d-flex w-100 h-100 justify-content-center align-items-center p-2">
+                <?php echo file_get_contents("./assets/img/PF_Icon_Longevity.svg"); ?>
             </div>
           </div>
-
+        </div>
+        <div class="line-down flex-fill">  </div>
+        <div class="general-card shadow py-2 px-3 mt-2 mb-4 w-75">
+          <img src="./assets/img/PW_PrimaryLogo_Dark_v01.svg" alt="Portfolio Waterfall Logo" class="img-fluid pb-3 px-4" />
+          <p class="text-center fst-italic px-3 fs-5 fw-bold lh-1 color--black">
+            Creates a Replenishing<br /> Lifetime Income
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
 </section>
