15e665be21636b268592c2b8686ed03bcde8c405
Jordan Lambrecht <jordan@pixelbakery.co>
2021-04-19 19:05:15 -0500

    consolidated code

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
   include_once("./inc/section_02_whatLimits.php");
   include_once("./inc/section_03_strategy.php");
   include_once("./inc/section_03_strategy_mobile.php");
-  include_once("./inc/section_05_piePipes.php");
+  include_once("./inc/section_04_piePipes.php");
   include_once("./inc/footer.php");
 ?>
